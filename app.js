b6b9516ab1d1ceb5090858c393be37195771b4a3
Rana Rashid <124416215+RashidAAleem@users.noreply.github.com>
2024-09-28 22:16:51 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// // var number = [1, 2, 5, 6, 7, 8, 9];
+// // var missingNumbers = [];
+
+// // var minNum = number[0];
+// // var maxNum = number[0];
+
+
+// // for (var i = 1; i < number.length; i++) {
+// //     if (number[i] < minNum) {
+// //         minNum = number[i];
+// //     }
+// //     console.log("min", minNum)
+    
+// //     if (number[i] > maxNum) {
+// //         maxNum = number[i];
+// //     }
+// // }
+
+// // // Check for missing numbers using nested loop
+// // for (var i = minNum; i <= maxNum; i++) {
+// //     var found = false; 
+// //     for (var j = 0; j < number.length; j++) {
+// //         if (number[j] === i) {
+// //             found = true; 
+// //             break; 
+// //         }
+// //     }
+// //     if (found === false) { 
+// //         missingNumbers[missingNumbers.length] = i; 
+// //     }
+// // }
+
+// // // Output the missing numbers
+// // document.write("Missing numbers: ");
+// // for (var k = 0; k < missingNumbers.length; k++) {
+// //     document.write(missingNumbers[k] + " ");
+// // }
